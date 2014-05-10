11a75c3b8ba2b11783fd84a77e02c78fece1f313
Isken Huang <kensheep@gmail.com>
2014-05-10 17:39:56 +0800

    update

---
+++ api/services/geocoder.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var Q = require('q'),
+    geocoder = require('geocoder');
+
+module.exports = {
+    // Geocoding
+    geocoder: function(address) {
+        var deferred = Q.defer();
+
+        geocoder.geocode( address, function ( err, data ) {
+            // do stuff with data
+            if(err){
+                deferred.reject(err);
+            }else{
+                deferred.resolve(data);
+            }
+        });
+
+        return deferred.promise;
+    },
+
+    reverseGeocode: function(latitude, longitude) {
+        var deferred = Q.defer();
+
+        geocoder.reverseGeocode( latitude, longitude, function ( err, data ) {
+            if(err){
+                deferred.reject(err);
+            }else{
+                deferred.resolve(data);
+            }
+        });
+
+        return deferred.promise;
+    },
+};
+
